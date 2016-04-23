774abd71b5e247aec6d7202d4285f8b6bb187cd9
kterf <edu.ribas.92@gmail.com>
2016-04-23 21:03:02 +0200

    4 esc control via serial

---
+++ 4-Outputs/ESC/fourESC.control.Serial/fourESC.control.Serial.ino
@@ -0,0 +1,107 @@
+/*
+*  This code is in the public domain.
+*  (Do whatever you want with it.)
+*/
+
+// Need the Servo library
+#include <Servo.h>
+
+// This is our motor.
+Servo myMotor1;
+Servo myMotor2;
+Servo myMotor3;
+Servo myMotor4;
+
+// This is the final output
+// written to the motor.
+String incomingString;
+
+
+// Set everything up
+void setup()
+{
+
+  myMotor1.attach(6);
+  myMotor2.attach(9);
+  myMotor3.attach(10);
+  myMotor4.attach(11);
+
+  // Required for I/O from Serial monitor
+  Serial.begin(38400);
+  // Print a startup message
+  Serial.println("initializing");
+}
+
+
+void loop()
+{
+  // If there is incoming value
+  if(Serial.available() > 0)
+  {
+    // read the value
+    char ch = Serial.read();
+  
+    /*
+    *  If ch isn't a newline
+    *  (linefeed) character,
+    *  we will add the character
+    *  to the incomingString
+    */
+    if (ch != 10){
+      // Print out the value received
+      // so that we can see what is
+      // happening
+      Serial.print("I have received: ");
+      Serial.print(ch, DEC);
+      Serial.print('\n');
+    
+      // Add the character to
+      // the incomingString
+      incomingString += ch;
+    }
+    // received a newline (linefeed) character
+    // this means we are done making a string
+    else
+    {
+      // print the incoming string
+      Serial.println("I am printing the entire string");
+      Serial.println(incomingString);
+    
+      // Convert the string to an integer
+      int val = incomingString.toInt();
+    
+      // print the integer
+      Serial.println("Printing the value: ");
+      Serial.println(val);
+    
+      /*
+      *  We only want to write an integer between
+      *  0 and 180 to the motor. 
+      */
+      if (val > -1 && val < 181)
+     {
+       // Print confirmation that the
+       // value is between 0 and 180
+       Serial.println("Value is between 0 and 180");
+       // Write to Servo
+       myMotor1.write(val);
+       myMotor2.write(val);
+       myMotor3.write(val-7);
+       myMotor4.write(val);
+     }
+     // The value is not between 0 and 180.
+     // We do not want write this value to
+     // the motor.
+     else
+     {
+       Serial.println("Value is NOT between 0 and 180");
+      
+       // IT'S a TRAP!
+       Serial.println("Error with the input");
+     }
+    
+      // Reset the value of the incomingString
+      incomingString = "";
+    }
+  }
+}
